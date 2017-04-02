ae35dd30f66ff1abf39d69e4630b6520bae12bae
Roald Hacquebord <roald@hacquebordbuckles.nl>
2017-04-02 16:54:22 +0200

    Add js transpiling and linter configs

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,11 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract([
           'css?sourceMap',
